f49078dd378ac1a3a4c51fbd15f9b804c2d1ac16
rhee-airilab <rhee.airilab@gmail.com>
2018-01-19 23:33:56 +0900

    Create cpinspect.py

---
+++ cpinspect.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+# cpinspect.py
+from __future__ import print_function, division, absolute_import
+import os
+import sys
+import tensorflow as tf
+
+def cpinspect(cp_or_dir):
+    l = tf.train.list_variables(cp_or_dir) # return list of (name, shape) tuples
+    return l
+
+if __name__ == '__main__':
+    l = cpinspect(sys.argv[1])
+    for name, shape in l:
+        print("'{}', # shape: {}".format(name,shape))
